0be648fb954d6edc1ebbe4b2b94ea62070a882ee
zhavei <zhavei.vei@gmail.com>
2021-09-19 22:14:15 +0700

    modifing xml layout gridview

--- app/src/main/java/com/zhavei/languangeapp/NumbersWordAdapater.java
+++ app/src/main/java/com/zhavei/languangeapp/NumbersWordAdapater.java
@@ -2,9 +2,11 @@ package com.zhavei.languangeapp;
 
 import android.app.Activity;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,6 +23,18 @@ public class NumbersWordAdapater extends ArrayAdapter<NumbersWord> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        View listItemView = convertView;
+        if (listItemView == null){
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.numbers_list_layout, parent, false);
+        }
+        NumbersWord currentNumbersList = getItem(position);
+        TextView listItemDefaultTranslation = (TextView) listItemView.findViewById(R.id.default_languange);
+        listItemDefaultTranslation.setText(currentNumbersList.getDefaultTrasnlation());
+
+        TextView indoTranslationList = (TextView) listItemView.findViewById(R.id.indo_languange_list);
+        indoTranslationList.setText(currentNumbersList.getLanguangeTranslation());
+
+        return listItemView;
     }
 }
