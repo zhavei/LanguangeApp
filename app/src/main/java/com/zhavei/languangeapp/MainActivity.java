0d4347d2a56fe6d2cf2716aac649af6345ebc22f
zhavei <zhavei.vei@gmail.com>
2021-10-10 22:51:35 +0700

    regulatif fix

--- app/src/main/java/com/zhavei/languangeapp/MainActivity.java
+++ app/src/main/java/com/zhavei/languangeapp/MainActivity.java
@@ -9,6 +9,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,78 +21,86 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
 
-        // Find the View that shows the numbers category
-        TextView numbers = (TextView) findViewById(R.id.numbers);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+        CategoryAdapter adapter = new CategoryAdapter(this, getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
 
-        // Set a click listener on that View
-        numbers.setOnClickListener(new OnClickListener() {
-            // The code in this method will be executed when the numbers category is clicked on.
-            @Override
-            public void onClick(View view) {
-                // Create a new intent to open the {@link NumbersActivity}
-                Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
-                // add toast message on click
-                Toast.makeText(view.getContext(), " Open List Of Numbers", Toast.LENGTH_SHORT).show();
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs_on_activity_main);
+        tabLayout.setupWithViewPager(viewPager);
 
-                // Start the new activity
-                startActivity(numbersIntent);
-            }
-        });
-
-        // Find the View that shows the family category
-        TextView family = (TextView) findViewById(R.id.family);
-
-        // Set a click listener on that View
-        family.setOnClickListener(new OnClickListener() {
-            // The code in this method will be executed when the family category is clicked on.
-            @Override
-            public void onClick(View view) {
-                // Create a new intent to open the {@link FamilyActivity}
-                Intent familyIntent = new Intent(MainActivity.this, FamilyActivity.class);
-                //add toast on click
-                Toast.makeText(view.getContext(), "Open Family List", Toast.LENGTH_SHORT).show();
-                // Start the new activity
-                startActivity(familyIntent);
-            }
-        });
-
-        // Find the View that shows the colors category
-        TextView colors = (TextView) findViewById(R.id.colors);
-
-        // Set a click listener on that View
-        colors.setOnClickListener(new OnClickListener() {
-            // The code in this method will be executed when the colors category is clicked on.
-            @Override
-            public void onClick(View view) {
-                // Create a new intent to open the {@link ColorsActivity}
-                Intent colorsIntent = new Intent(MainActivity.this, ColorsActivity.class);
-                //add toast on click
-                Toast.makeText(view.getContext(), " open colors list", Toast.LENGTH_SHORT).show();
-                // Start the new activity
-                startActivity(colorsIntent);
-            }
-        });
-
-        // Find the View that shows the phrases category
-        TextView phrases = (TextView) findViewById(R.id.phrases);
-
-        // Set a click listener on that View
-        phrases.setOnClickListener(new OnClickListener() {
-            // The code in this method will be executed when the phrases category is clicked on.
-            @Override
-            public void onClick(View view) {
-                // Create a new intent to open the {@link PhrasesActivity}
-                Intent phrasesIntent = new Intent(MainActivity.this, PhrasesActivity.class);
-                //add toast on click
-                Toast.makeText(view.getContext(), "open list phrases", Toast.LENGTH_SHORT).show();
-                // Start the new activity
-                startActivity(phrasesIntent);
-            }
-        });
+//        // Set the content of the activity to use the activity_main.xml layout file
+//        setContentView(R.layout.activity_main);
+//
+//        // Find the View that shows the numbers category
+//        TextView numbers = (TextView) findViewById(R.id.numbers);
+//
+//        // Set a click listener on that View
+//        numbers.setOnClickListener(new OnClickListener() {
+//            // The code in this method will be executed when the numbers category is clicked on.
+//            @Override
+//            public void onClick(View view) {
+//                // Create a new intent to open the {@link NumbersActivity}
+//                Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
+//                // add toast message on click
+//                Toast.makeText(view.getContext(), " Open List Of Numbers", Toast.LENGTH_SHORT).show();
+//
+//                // Start the new activity
+//                startActivity(numbersIntent);
+//            }
+//        });
+//
+//        // Find the View that shows the family category
+//        TextView family = (TextView) findViewById(R.id.family);
+//
+//        // Set a click listener on that View
+//        family.setOnClickListener(new OnClickListener() {
+//            // The code in this method will be executed when the family category is clicked on.
+//            @Override
+//            public void onClick(View view) {
+//                // Create a new intent to open the {@link FamilyActivity}
+//                Intent familyIntent = new Intent(MainActivity.this, FamilyActivity.class);
+//                //add toast on click
+//                Toast.makeText(view.getContext(), "Open Family List", Toast.LENGTH_SHORT).show();
+//                // Start the new activity
+//                startActivity(familyIntent);
+//            }
+//        });
+//
+//        // Find the View that shows the colors category
+//        TextView colors = (TextView) findViewById(R.id.colors);
+//
+//        // Set a click listener on that View
+//        colors.setOnClickListener(new OnClickListener() {
+//            // The code in this method will be executed when the colors category is clicked on.
+//            @Override
+//            public void onClick(View view) {
+//                // Create a new intent to open the {@link ColorsActivity}
+//                Intent colorsIntent = new Intent(MainActivity.this, ColorsActivity.class);
+//                //add toast on click
+//                Toast.makeText(view.getContext(), " open colors list", Toast.LENGTH_SHORT).show();
+//                // Start the new activity
+//                startActivity(colorsIntent);
+//            }
+//        });
+//
+//        // Find the View that shows the phrases category
+//        TextView phrases = (TextView) findViewById(R.id.phrases);
+//
+//        // Set a click listener on that View
+//        phrases.setOnClickListener(new OnClickListener() {
+//            // The code in this method will be executed when the phrases category is clicked on.
+//            @Override
+//            public void onClick(View view) {
+//                // Create a new intent to open the {@link PhrasesActivity}
+//                Intent phrasesIntent = new Intent(MainActivity.this, PhrasesActivity.class);
+//                //add toast on click
+//                Toast.makeText(view.getContext(), "open list phrases", Toast.LENGTH_SHORT).show();
+//                // Start the new activity
+//                startActivity(phrasesIntent);
+//            }
+//        });
     }
 
     @Override
